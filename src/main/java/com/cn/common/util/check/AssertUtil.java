f9d7fd18a60c6372e290b38060ad18c20b6439a9
罗恒心 <924666778@qq.com>
2019-12-03 20:28:47 +0800

    优化 断言工具类

--- src/main/java/com/cn/common/util/check/AssertUtil.java
+++ src/main/java/com/cn/common/util/check/AssertUtil.java
@@ -1,10 +1,9 @@
 package com.cn.common.util.check;
 
-public class CheckUtil {
-
+public class AssertUtil {
 
     public static void check(boolean result,RuntimeException e){
-        if(result){
+        if(!result){
             throw e;
         }
     }
